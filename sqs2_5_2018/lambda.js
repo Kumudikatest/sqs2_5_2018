77cd1795fd12513be59de5ab757dae172f150735
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-05-03 14:27:57 +0530

    test

--- sqs2_5_2018/lambda.js
+++ sqs2_5_2018/lambda.js
@@ -1,6 +1,17 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+exports.handler = function (event, context, callback) {
+	cognito_idp.adminDisableUser({
+		UserPoolId: "us-east-1_uVXTQInep", /* required */
+		Username: "n" /* required */
+	}, function (error, data) {
+		if (error) {
+			// implement error handling logic here
+			throw error;
+		}
+		// your logic goes within this block
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
